9987693331d65b7cdbfcffd42a780b0e3f835498
Fred Hsieh <fhsieh@uci.edu>
2022-12-23 13:17:29 -0800

    Write default code for init script

--- aoc_init.py
+++ aoc_init.py
@@ -10,7 +10,26 @@ except ImportError:
     sys.exit(1)
 
 
+def build_folder(day):
+    # create folder
+    folder = "day-{}".format(day)
+    print("Creating folder: {}".format(folder))
+    # create files
+    files = ["test.txt", "input.txt", "{}.py".format(day)]
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        print("Folder {} already exists".format(folder))
+    for file in files:
+        try:
+            open(os.path.join(folder, file), "w")
+        except FileExistsError:
+            print("File {} already exists".format(file))
+    return folder
+
+
 def download(day, year, path):
+    print("Downloading input and example data...")
     # check if session key is set
     if "AOC_SESSION" not in os.environ:
         token = subprocess.check_output(["aocd-token".format(day, year)])
@@ -29,6 +48,18 @@ def download(day, year, path):
         f.write(puzzle.input_data)
 
 
+def write_default_code(day):
+    with open("day-{}/{}.py".format(day, day), "w") as f:
+        f.write("# Path: day-{}/{}.py\n".format(day, day))
+        f.write("# Solution for day {} of Advent of Code\n".format(day))
+        f.write("import os\n\nfrom aoc_tools import Test\n\n\n")
+        f.write("def read_data(file):\n\tpass\n\n\n")
+        f.write("def solve(data):\n\tpass\n\n\n")
+        f.write('if __name__ == "__main__":\n')
+        f.write("\tos.chdir(os.path.dirname(os.path.abspath(__file__)))\n")
+        f.write('\ttest, input = read_data("test.txt"), read_data("input.txt")\n')
+
+
 def main(args):
     if len(args) != 3:
         print("Usage: python setup.py <year> <day>")
@@ -36,24 +67,19 @@ def main(args):
     # setup a new day for aoc
     year = int(args[1])
     day = int(args[2])
+    # Check if folder exists
+    if os.path.exists("day-{}".format(day)):
+        print("Day {} already exists".format(day))
+        sys.exit(1)
     print("Creating new day of AoC: {}-12-{}".format(year, day))
-    # create folder
-    folder = "day-{}".format(day)
-    print("Creating folder: {}".format(folder))
-    # create files
-    files = ["test.txt", "input.txt", "{}.py".format(day)]
-    try:
-        os.mkdir(folder)
-    except FileExistsError:
-        print("Folder {} already exists".format(folder))
-    for file in files:
-        try:
-            open(os.path.join(folder, file), "w")
-        except FileExistsError:
-            print("File {} already exists".format(file))
-    # download input and test
-    print("Downloading input and test...")
+
+    # Create folder
+    folder = build_folder(day)
+    # Download input and test data
     download(day, year, folder)
+    # write default code for day
+    write_default_code(day)
+
     print("Done! Happy coding!")
 
 
---
+++ day-19/19.py
@@ -0,0 +1,18 @@
+# Path: day-19/19.py
+# Solution for day 19 of Advent of Code
+import os
+
+from aoc_tools import Test
+
+
+def read_data(file):
+    pass
+
+
+def solve(data):
+    pass
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    test, input = read_data("test.txt"), read_data("input.txt")
